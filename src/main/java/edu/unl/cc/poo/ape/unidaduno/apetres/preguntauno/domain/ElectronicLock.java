49b96e0d412e9534651c9bc4bb28c0bc3a3ea55f
Javier Guarnizo <javierguarnizo11@gmail.com>
2026-05-04 10:40:38 -0500

    Creando las clases de la pregunta 1

---
+++ src/main/java/edu/unl/cc/poo/ape/unidaduno/apetres/preguntauno/domain/ElectronicLock.java
@@ -0,0 +1,4 @@
+package edu.unl.cc.poo.ape.unidaduno.apetres.preguntauno.domain;
+
+public class ElectronicLock {
+}
---
+++ src/main/java/edu/unl/cc/poo/ape/unidaduno/apetres/preguntauno/view/ElectronicLockExecutor.java
@@ -0,0 +1,4 @@
+package edu.unl.cc.poo.ape.unidaduno.apetres.preguntauno.view;
+
+public class ElectronicLockExecutor {
+}
